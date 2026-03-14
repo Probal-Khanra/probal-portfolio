e49fb9f971ed6ada3d1faf44e420475bdf8be66a
Probal <probal.builds@gmail.com>
2026-03-14 21:19:15 +0530

    Design: Update title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Probal Khanra | Electronics Engineer",
+  title: "Probal Khanra | Electrical Engineer",
   description: "Portfolio of Probal Khanra - Focused on Embedded Systems and IoT.",
 };
 
